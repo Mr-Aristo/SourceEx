865335e9d421570ace0eeefa9fcd339d29894d03
Emre <Emre@DESKTOP-8FMJR1I>
2026-02-27 18:56:06 +0300

    #2 Domain layer implemented - Aggregate root

--- src/BuildingBlocks/CQRS/IQuery.cs
+++ src/BuildingBlocks/CQRS/IQuery.cs
@@ -5,6 +5,11 @@ namespace BuildingBlocks.CQRS;
 /// </summary>
 /// <typeparam name="TResponse">The type of the response returned by the query. It cannot be null.</typeparam>
 
+/// <summary>
+/// Represents a query in the CQRS architecture. 
+/// Queries are used to retrieve data from the database without modifying the system's state.
+/// </summary>
+/// <typeparam name="TResponse">The type of the response returned by the query. It cannot be null.</typeparam>
 public interface IQuery<TResponse> : IRequest<TResponse> where TResponse :notnull
 {
 }
