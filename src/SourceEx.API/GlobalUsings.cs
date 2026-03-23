6f9780208a95225cea95077a4f1bb52f5defb634
Emre Gecher <e.gecher@redbrixwall.com>
2026-03-23 13:59:23 +0300

    Refctor usings

---
+++ src/SourceEx.API/GlobalUsings.cs
@@ -0,0 +1,33 @@
+global using Asp.Versioning;
+global using Asp.Versioning.Builder;
+
+global using BuildingBlocks.Messaging;
+global using BuildingBlocks.Observability;
+global using BuildingBlocks.Security;
+global using BuildingBlocks.Security;
+
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Microsoft.AspNetCore.HttpOverrides;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Diagnostics;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.RateLimiting;
+
+global using Prometheus;
+global using Serilog;
+global using Serilog.Events;
+
+global using System.Security.Claims;
+global using System.Threading.RateLimiting;
+
+global using SourceEx.API.Endpoints;
+global using SourceEx.API.ExceptionHandling;
+global using SourceEx.API.Observability;
+global using SourceEx.API.RateLimiting;
+global using SourceEx.API.Security;
+global using SourceEx.API.Contracts;
+global using SourceEx.Application;
+global using SourceEx.Infrastructure.Bootstrap;
+global using SourceEx.Infrastructure;
+global using SourceEx.Application.Exceptions;
+global using SourceEx.Domain.Exceptions;
